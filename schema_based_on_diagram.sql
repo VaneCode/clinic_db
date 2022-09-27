0105d6a212dbb755db5d9b5f0182498fafd49cd9
Apuabi Titilope <tyaps002@gmail.com>
2022-09-27 20:32:55 +0100

    Create the patients and treatments tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,5 +1,16 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE patients (
+  id int GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(200),
+  date_of_birth DATE,
+  PRIMARY KEY(id)
 );
+
+CREATE TABLE treatments (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  type VARCHAR(20),
+  name VARCHAR(100),
+  PRIMARY KEY(id)
+);
+
