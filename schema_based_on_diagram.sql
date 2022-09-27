430457ae1e9a56051b146182aafbe91e50ae8b8b
Ijeoma Odinko <95123554+ijeomaodinko@users.noreply.github.com>
2022-09-27 15:37:52 -0400

    Add tables for medical_histories and invoices

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -14,3 +14,19 @@ CREATE TABLE treatments (
   PRIMARY KEY(id)
 );
 
+CREATE TABLE medical_histories (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  admited_at TIMESTAMP,
+  patient_id INT REFERENCES patients(id),
+  status VARCHAR(10),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE invoices (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  total_amount DECIMAL,
+  generated_at TIMESTAMP,
+  payed_at TIMESTAMP,
+  medical_histories_id INT REFERENCES medical_histories(id),
+  PRIMARY KEY(id)
+);
